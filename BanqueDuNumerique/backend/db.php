15f716db2c7aa8db19750cba42e4282ffec3dc67
AOUADENE Adel <aouadeneadel1@gmail.com>
2025-09-18 17:21:57 +0200

    Delete BanqueDuNumerique directory

--- BanqueDuNumerique/backend/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-// db.php
-return array(
-    'db_host' => 'localhost',
-    'db_name' => 'banque_numerique',
-    'db_user' => 'root',
-    'db_pass' => '',
-    'db_charset' => 'utf8mb4',
-);
