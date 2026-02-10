262e587cd56f5b14d18c6351a7baf543833e3c11
AOUADENE Adel <aouadeneadel@outlook.fr>
2026-02-10 18:00:07 +0100

    Add function to download PDF files

---
+++ script.js
@@ -0,0 +1,8 @@
+function downloadPDF(filename) {
+    const link = document.createElement("a");
+    link.href = filename;
+    link.download = filename;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
